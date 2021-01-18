14bc7a42ec840d28b6202a84070c62a8542c9ea8
Amber Mattis <amber.mattis2@gmail.com>
2021-01-18 16:06:06 -0600

    added function that will display moday when create a new list button is clicked

--- td-list.js
+++ td-list.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
     "use strict"
 //    create a function that changes the title of the current list
-//    this will change to main headind
+//    this will change to main heading
 
 
+// This function will display a modal that should allow the user to change the name of the to-do list
+function changeListTitle() {
+    $("#newList").click(function () {
+        $(".modal").modal("show");
+    });
+}
 
-
-
-
-
-
+// will allow the user to click create new list button on page load
+$(window).onload(changeListTitle());
 
 })
